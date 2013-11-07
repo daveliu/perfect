b6e72c636b22cc201172f9c3f7dabe312c3f2991
daveliu <dave@liageren.com>
2013-11-07 17:12:18 +0800

    change puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,14 @@
-APP_ROOT = '/home/dave/www/perfect/current'
+APP_ROOT = '/Users/dave/homebrew/code/perfect'
 pidfile "#{APP_ROOT}/tmp/pids/puma.pid"
 state_path "#{APP_ROOT}/tmp/pids/puma.state"
+
+railsenv = 'production'
+
+stdout_redirect "#{APP_ROOT}/log/puma-#{railsenv}.stdout.log", "#{APP_ROOT}/log/puma-#{railsenv}.stderr.log"
+
 daemonize true
 workers 4
 threads 8,32
 preload_app!
+
+bind "unix://#{APP_ROOT}/tmp/#{railsenv}.socket"
